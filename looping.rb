6c50dee4423ed47fc26757979c1991cc1f8114ca
ryosyntaxx <riodwi12174@gmail.com>
2024-02-14 15:39:43 +0700

    looping

---
+++ looping.rb
@@ -0,0 +1,15 @@
+# jika menggunaakan angka bisa menggunakan fitur times
+
+5.times do
+    puts 'hello'
+end
+
+# kurung kurawal hanya bisa 1 line dan 1 code
+
+3.times { puts 'hi' }
+
+# untuk mengambil index ke berapa
+
+4.times do |index|
+    puts "index: #{index}"
+end
